e20ad6f11cf5617cde197f902cb621a2dfb39a2e
Anum Qudsia <anumqudsia@hotmail.com>
2013-03-10 16:08:18 +0000

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Lc::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w[ *.css ]
+    config.assets.precompile += %w[ '*.css' ]
+	config.assets.precompile += %w[ 'learn_main.css', 'mainskill.css', 'reset.css', 'signup.css' ]
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
